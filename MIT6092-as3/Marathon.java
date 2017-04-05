4d6c547c295217a09eb212e8c4981b54c7927921
artijadhav984 <artijadhav984@gmail.com>
2017-04-05 14:37:39 -0700

    Updated GetFirstTwoMinIndices method for handling empty array and repeated min values

--- MIT6092-as3/Marathon.java
+++ MIT6092-as3/Marathon.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 /*
  * Assignment 3: Marathon
  * Name: Arti
  * Created date: 04 Apr 2017
- * Last updted: 04 Apr 2017
+ * Last updted: 05 Apr 2017
  */
 class Marathon {
     public static void main (String[] arguments) {
@@ -35,34 +37,68 @@ class Marathon {
         	System.out.println("Second fastest runner is " + names[secondMinIndex] + ": " + times[secondMinIndex]);
         }
         
-        int[] firstTwoMinIndices = GetFirstTwoMinIndices(times);
-        System.out.println("Fastest runners are:");
+        // Test GetFirstTwoMinIndices method for empty array
+        int[][] newTimes = {times, { }, { 1, 0, 0, 1, 1 }, { 0, 0 }, { 0 }};
         
-        for(int i = 0; i < firstTwoMinIndices.length; i++)
+        for(int j = 0; j < newTimes.length; j++)
         {
-        	if(firstTwoMinIndices[i] != -1)
-        	{
-        		System.out.println(i+1 + ". " + names[firstTwoMinIndices[i]] + ": " + times[firstTwoMinIndices[i]]);
-        	}
-        }
+        	int[][] firstTwoMinIndices = GetFirstTwoMinIndices(newTimes[j]);
         
+	        if(firstTwoMinIndices[0] != null)
+	        {
+	            System.out.println("Fastest runners are:");
+	            
+		        for(int i = 0; i < firstTwoMinIndices[0].length; i++)
+		        {
+		        	if(firstTwoMinIndices[0][i] != -1)
+		        	{
+		        		System.out.println("1. " + names[firstTwoMinIndices[0][i]] + ": " + newTimes[j][firstTwoMinIndices[0][i]]);
+		        	}
+		        	else
+		        		break;
+		        }
+		        
+		        for(int i = 0; i < firstTwoMinIndices[1].length; i++)
+		        {
+		        	if(firstTwoMinIndices[1][i] != -1)
+		        	{
+		        		System.out.println("2. " + names[firstTwoMinIndices[1][i]] + ": " + newTimes[j][firstTwoMinIndices[1][i]]);
+		        	}
+		        	else
+		        		break;
+		        }
+	        }
+	        else
+	        {
+	        	System.out.println("There are no runners !");
+	        }
+        }
     }
     
     /*
      * Method to get indices of first two minimum values
      * int[] times: array of integer values to be compared
      */
-    static int[] GetFirstTwoMinIndices(int[] times)
+    static int[][] GetFirstTwoMinIndices(int[] times)
     {
-    	int[] firstTwoMinIndices = new int[2];
+    	if(times.length == 0)
+		{
+			return new int[2][];
+		}
+		
+    	int[][] firstTwoMinIndices = new int[2][times.length];
     	
     	try 
     	{
+    		Arrays.fill(firstTwoMinIndices[0], -1);
+        	Arrays.fill(firstTwoMinIndices[1], -1);
     		int minValIndex = -1;
         	int secondMinValIndex = -1;
     		Integer minVal = null;
     		Integer secondMinVal = null;
-    		
+    		int minValCount = 0;
+			int secondMinValCount = 0;
+			
     		for (int i = 0; i < times.length; i++) 
     		{   
     			if(minVal == null || minVal > times[i])
@@ -76,15 +112,35 @@ class Marathon {
                 	minVal = times[i];
                 	minValIndex = i;
                 }
-    			else if((secondMinVal == null || secondMinVal > times[i]) && (minValIndex != i))
+    			else if((secondMinVal == null || secondMinVal > times[i]) && (minVal != times[i]))
     			{
     				secondMinVal = times[i];
     				secondMinValIndex = i;
     			}
             }
     		
-    		firstTwoMinIndices[0] = minValIndex;
-    		firstTwoMinIndices[1] = secondMinValIndex;
+    		firstTwoMinIndices[0][minValCount++] = minValIndex;
+    		firstTwoMinIndices[1][secondMinValCount++] = secondMinValIndex;
+    		
+    		if(minVal != null || secondMinVal != null)
+    		{
+    			for (int i = 0; i < times.length; i++) 
+        		{   
+    				if(i == minValIndex || i == secondMinValIndex)
+    				{
+    					continue;
+    				}
+    				
+    				if(times[i] == minVal)
+    				{
+    		    		firstTwoMinIndices[0][minValCount++] = i;
+    				}
+    				else if(times[i] == secondMinVal)
+    				{
+    		    		firstTwoMinIndices[1][secondMinValCount++] = i;
+    				}
+        		}
+    		}
     	}
 		catch(Exception ex)
 		{
